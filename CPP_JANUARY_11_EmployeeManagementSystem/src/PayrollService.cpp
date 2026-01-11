33ca7fa6a9c3a44f3408f07ef6f9b5a197f86a65
Mikael <akopyanmikael88@gmail.com>
2026-01-11 21:52:22 +0400

    add new logic

--- CPP_JANUARY_11_EmployeeManagementSystem/src/PayrollService.cpp
+++ CPP_JANUARY_11_EmployeeManagementSystem/src/PayrollService.cpp
@@ -4,7 +4,7 @@
 #include "Manager.h"
 #include "SalesPerson.h"
 
-void PayrollService::giveSalary(const std::shared_ptr<Employee>& e) const noexcept{
-    if (e->hasSalary()) std::cout << "Successfuly!" << std::endl;
+void PayrollService::giveSalary(const std::shared_ptr<Employee>& emp) const noexcept{
+    if (dynamic_cast<const Accountant*>(emp.get()) || dynamic_cast<const Developer*>(emp.get())  || dynamic_cast<const Manager*>(emp.get()) || dynamic_cast<const SalesPerson*>(emp.get())) std::cout << "Successfuly!" << std::endl;
     else std::cout << "King don't need salary" << std::endl;
 }
